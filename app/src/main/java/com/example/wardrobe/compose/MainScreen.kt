ac91c5cfd833bda67b68129147f849d9c4e2df11
chenkexi <kexi_chen@huace.cn>
2023-11-13 18:14:19 +0800

    增加刮刮乐背景

--- app/src/main/java/com/example/wardrobe/compose/MainScreen.kt
+++ app/src/main/java/com/example/wardrobe/compose/MainScreen.kt
@@ -1,47 +1,30 @@
 package com.example.wardrobe.compose
 
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import android.graphics.Path
+import android.view.MotionEvent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.PaintingStyle
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.wardrobe.viewmodel.MainScreenViewModel
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.example.wardrobe.R
-import com.example.wardrobe.compose.ui.custom.FlipCard
-
 /**
  * 作用：
  *
@@ -60,7 +43,8 @@ fun MainScreen(
     onBackClick: () -> Unit
 ) {
     Surface {
-        FlipCard()
+        MyApp()
+        //FlipCard()
         Button(onClick = onBackClick) {
             Text(text = "button")
 
@@ -68,3 +52,86 @@ fun MainScreen(
     }
 
 }
+
+@Composable
+fun PrizeGrid(prizes: List<String>, modifier: Modifier = Modifier) {
+    GridLayout(prizes = prizes, modifier = modifier)
+}
+
+@Composable
+fun GridLayout(prizes: List<String>, modifier: Modifier) {
+    // 使用 Column 和 Row 创建网格布局
+    Column(modifier = modifier) {
+        for (i in prizes.indices step 5) {
+            Row {
+                for (j in 0 until 5) {
+                    if (i + j < prizes.size) {
+                        Box(modifier = Modifier
+                            .weight(1f)
+                            .padding(4.dp),
+                            contentAlignment = Alignment.Center) {
+                            Text(text = prizes[i + j] ,
+                                textAlign = TextAlign.Center,
+                                fontSize = 16.sp,
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScratchCard(prizes: List<String>) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        PrizeGrid(prizes = prizes)
+//        ScratchOffCanvas {
+//            // 这里可以添加额外的逻辑，比如记录刮开的位置等
+//        }
+    }
+}
+@Composable
+fun MyApp() {
+    val prizes = listOf("100元", "50元", "20元", "10元", "5元") // 示例奖金
+    ScratchCard(prizes = prizes)
+}
+
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun ScratchOffCanvas(modifier: Modifier = Modifier, onScratch: (Offset) -> Unit) {
+    val path = Path()
+    val paint = Paint().apply {
+        color = Color.Red
+        style = PaintingStyle.Fill
+    }
+
+    Canvas(modifier = modifier
+        .fillMaxSize()
+        .pointerInteropFilter { event ->
+            when (event.action) {
+                MotionEvent.ACTION_DOWN,
+                MotionEvent.ACTION_MOVE -> {
+                    path.addCircle(
+                        event.x,
+                        event.y,
+                        30f, // 擦除半径
+                        Path.Direction.CCW
+                    )
+                    onScratch(Offset(event.x, event.y))
+                    true
+                }
+                else -> false
+            }
+        }) {
+        drawRect(
+            color = Color.Blue,
+            size = this.size
+        )
+        drawContext.canvas.nativeCanvas.drawPath(path, paint.asFrameworkPaint())
+    }
+}
+
+
---
+++ app/src/main/java/com/example/wardrobe/compose/ui/custom/LuckyCard.kt
@@ -0,0 +1,247 @@
+package com.example.wardrobe.compose.ui.custom
+
+import android.annotation.SuppressLint
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.wardrobe.R
+import com.example.wardrobe.compose.ui.theme.WardrobeTheme
+
+/**
+ * 作用：刮刮乐卡片
+ *
+ * @author chenkexi
+ * @date :2023/11/13
+ */
+
+@SuppressLint("ResourceType")
+@Composable
+fun LuckyCard() {
+    var rotated by remember { mutableStateOf(false) }
+    val rotation by animateFloatAsState(
+        targetValue = if (rotated) 180f else 0f,
+        animationSpec = tween(500), label = ""
+    )
+    val animateFront by animateFloatAsState(
+        targetValue = if (!rotated) 1f else 0f,
+        animationSpec = tween(500), label = ""
+    )
+    val animateBack by animateFloatAsState(
+        targetValue = if (rotated) 1f else 0f,
+        animationSpec = tween(500), label = ""
+    )
+    Card(
+        modifier = Modifier
+            .height(600.dp)
+            .fillMaxWidth()
+            .padding(20.dp)
+            .graphicsLayer {
+                rotationY = rotation
+                cameraDistance = 8 * density
+            }
+            .clickable {
+                rotated = !rotated
+            },
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp
+        ),
+        shape = RoundedCornerShape(14.dp)
+    ) {
+        if (!rotated) {
+
+            Box(Modifier.fillMaxSize()){
+                Image(
+                    painter = painterResource(R.drawable.card_back1),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(14.dp)),
+                    contentScale = ContentScale.FillBounds
+                )
+
+            }
+
+            Box (Modifier.fillMaxSize().background(Color.Transparent)){
+
+            }
+
+//            Column(
+//                horizontalAlignment = Alignment.Start,
+//                verticalArrangement = Arrangement.SpaceBetween,
+//                modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+//            ) {
+//                Row(horizontalArrangement = Arrangement.SpaceBetween) {
+//                    Icon(
+//                        painter = painterResource(R.drawable.welcome_background),
+//                        contentDescription = "test",
+//                        modifier = Modifier
+//                            .width(50.dp)
+//                            .height(50.dp)
+//                            .padding(top = 6.dp, bottom = 6.dp, end = 20.dp)
+//                            .graphicsLayer {
+//                                alpha = animateFront
+//                            },
+//                        tint = Color.White
+//                    )
+//                    Spacer(modifier = Modifier.weight(1f))
+//                    Image(
+//                        painter = cardType,
+//                        contentDescription = "test",
+//                        modifier = Modifier
+//                            .width(50.dp)
+//                            .height(50.dp)
+//                            .graphicsLayer {
+//                                alpha = animateFront
+//                            }
+//                    )
+//                }
+//                Text(
+//                    text = "hhhhhh",
+//                    modifier = Modifier
+//                        .padding(top = 16.dp)
+//                        .graphicsLayer {
+//                            alpha = animateFront
+//                        },
+//                    fontWeight = FontWeight.Normal,
+//                    fontSize = 25.sp
+//                )
+//
+//                Row(horizontalArrangement = Arrangement.SpaceBetween) {
+//                    Column(horizontalAlignment = Alignment.Start) {
+//                        Text(
+//                            text = "Card Holder",
+//                            color = Color.Gray,
+//                            fontSize = 9.sp,
+//                            fontWeight = FontWeight.Bold,
+//                            modifier = Modifier
+//                                .graphicsLayer {
+//                                    alpha = animateFront
+//                                }
+//                        )
+//                        Text(
+//                            text = "Mehmet Yozgatli",
+//                            color = Color.White,
+//                            fontSize = 15.sp,
+//                            fontWeight = FontWeight.Bold,
+//                            modifier = Modifier
+//                                .graphicsLayer {
+//                                    alpha = animateFront
+//                                }
+//                        )
+//                    }
+//                    Spacer(modifier = Modifier.weight(1f))
+//                    Column(horizontalAlignment = Alignment.Start) {
+//                        Text(
+//                            text = "VALID THRU",
+//                            color = Color.Gray,
+//                            fontSize = 9.sp,
+//                            fontWeight = FontWeight.Bold,
+//                            modifier = Modifier
+//                                .graphicsLayer {
+//                                    alpha = animateFront
+//                                }
+//                        )
+//
+//                        Text(
+//                            text = "ttttttt",
+//                            color = Color.White,
+//                            fontSize = 15.sp,
+//                            fontWeight = FontWeight.Bold,
+//                            modifier = Modifier
+//                                .graphicsLayer {
+//                                    alpha = animateFront
+//                                }
+//                        )
+//
+//                    }
+//                }
+//            }
+        } else {
+            Column(
+                modifier = Modifier.padding(top = 20.dp),
+            ) {
+                Divider(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            alpha = animateBack
+                        }, color = Color.Black, thickness = 50.dp
+                )
+                Text(
+                    text = "123",
+                    color = Color.Black,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .background(Color.White)
+                        .fillMaxWidth()
+                        .graphicsLayer {
+                            alpha = animateBack
+                            rotationY = rotation
+                        }
+                        .padding(10.dp),
+                    fontSize = 15.sp,
+                    textAlign = TextAlign.End
+                )
+                Text(
+                    text = "Developed by Mehmet Yozgatli",
+                    color = Color.White,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .graphicsLayer {
+                            alpha = animateBack
+                            rotationY = rotation
+                        }
+                        .padding(5.dp),
+                    fontWeight = FontWeight.Thin,
+                    fontSize = 10.sp,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LuckyCardPreview() {
+    WardrobeTheme {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Transparent)
+                .background(Color.White, RoundedCornerShape(20.dp, 20.dp))
+        ) {
+            LuckyCard()
+        }
+    }
+}
